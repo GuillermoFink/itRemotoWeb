802ba1221c315625b304018f043ee68065cb0b1b
Guillermo Fink (CM) <guillermo.fink@ar.vision101.corp>
2018-09-06 14:25:20 -0300

    iconos de menu y load de faicons

--- src/app/componentes/menu/menu.component.ts
+++ src/app/componentes/menu/menu.component.ts
@@ -14,28 +14,26 @@ export class MenuComponent implements OnInit {
   ngOnInit() {
     this.items = [
       {
-          label: 'File',
-          items: [{
-                  label: 'New', 
-                  icon: 'pi pi-fw pi-plus',
-                  items: [
-                      {label: 'Project'},
-                      {label: 'Other'},
-                  ]
-              },
-              {label: 'Open'},
-              {label: 'Quit'}
-          ]
+        label: 'Usuarios',
+        icon: 'fa fa-users',
+        items: [
+        {
+          label: 'Crear nuevo usuario',
+          icon: 'pi pi-fw pi-plus',
+        },
+        { label: 'Ver Usuarios', icon: 'fas fa-users-cog' },
+        { label: 'Auditoria', icon: 'fas fa-user-ninja' }
+        ]
       },
       {
-          label: 'Edit',
-          icon: 'pi pi-fw pi-pencil',
-          items: [
-              {label: 'Delete', icon: 'pi pi-fw pi-trash'},
-              {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
-          ]
+        label: 'Edit',
+        icon: 'pi pi-fw pi-pencil',
+        items: [
+          { label: 'Delete', icon: 'pi pi-fw pi-trash' },
+          { label: 'Refresh', icon: 'pi pi-fw pi-refresh' }
+        ]
       }
-  ];
+    ];
   }
 
 }
